03df0af8c93284b883adc975d97a236e48c5cc58
Dev Squid <contact.devsquid@gmail.com>
2016-03-02 12:59:50 -0600

    added start to documentation of code

--- server/server.py
+++ server/server.py
@@ -4,6 +4,16 @@
 !@ OUTGOING REQUESTS FOR COLORED TEXT IN CONSOLE ARE LABELED AS COLOR :: !@
 !@ EXAMPLE "::RED:: username ::WHITE:: joins the server"
 """
+# (C) Ryan Schaefer 2016
+# This is module meant to be run in conjunction with a server "handler"
+# ---------------------------------------------------------------------
+# .
+# .
+# .
+# .
+# GLHF implementing this
+
+#Standars imports I don't need to explain this
 import random
 import socket
 import pickle
@@ -11,28 +21,37 @@ import os
 import console
 import json
 from time import sleep
-#!@ TODO MAKE ITEM / RESOURCE (ITEMS ARE SINGULAR AND RESOURCES ARE MUTLIPLE)
+#Resources class : multiple objects used for crafting
 class Resource(object):
+	#every resource shares same characteristic overriden per specific resource
 	name = None
 	extract_time = 0
 	def __init__(self):
-		self.amount = 0
+		# each instance needs its own amount
+		self.amount = 0		
 class Wood(Resource):
 	name = "wood"
+	extract_time = 5	
 class Stone(Resource):
 	name = "stone"
+	extract_time = 10
 class Water(Resource):
 	name = "water"
+	extract_time = 1
 class Sand(Resource):
 	name = "sand"
+	extract_time = 4
+#Items do not stack / there is only one of them at a time
 class Item(object):
 	def __init__(self):
-		#!@ TODO FIX ITEM ID / NAME / AMOUNT
+		#!@ TODO IMPLEMENT ITEM ID / NAME / AMOUNT
 		self.id = 0
 		self.amount = 1
 		self.name = ""
 	def use(self):
+		# not implemented yet
 		print "this fuction uses the item"
+#One square of a map; has resources / items / players / avialable moves from it / a position on the map
 class Square(object):
 	def __init__(self):
 		self.items    = {}
@@ -42,20 +61,24 @@ class Square(object):
 		self.resources = []
 		self.pos = None
 	def update_moves(self, pos_x, pos_y):
+		#called when adding to the map, when a player is on it it governs where the player can move to
 		self.moves = {"left" : str(pos_x - 1) + ":" + str(pos_y), "up_left" : str(pos_x - 1) + ":" + str(pos_y - 1), "down_left" : str(pos_x - 1) + ":" + str(pos_y + 1), "right" : str(pos_x + 1) + ":" + str(pos_y), "up_right" :str(pos_x + 1) + ":" + str(pos_y - 1), "down_right":str(pos_x + 1) + ":" + str(pos_y + 1), "up": str(pos_x) + ":" + str(pos_y - 1), "down": str(pos_x) + ":" + str(pos_y + 1)}
 		self.pos = str(pos_x)+ ":" +str(pos_y)
 	def describe(self):
 		print "This is a basic description of a square, update it when you make a square."
 	def extract(self, resource, player):
+		# extraction can only be one resource at a time currently time is defined by the resource that is being extracted
 		if self.resources[resource.name] > 0:
 			sleep(resource.extract_time)
 			self.resources[resource.name].amount -= 1
 			player.add_resource(resource, 1)
 	def add_resource(self, type, amount):
+		#adds a resource to the square
 		for resoure in resources:
 			if resource.name == type.name:
 				resource.amount += amount
 	def init_resources(self):
+		#changes what resources are avialable to be extracted
 		self.resources = []
 class Job(object):
 	pass
@@ -70,10 +93,11 @@ class Player(object):
 		self.group          = None
 		self.moves = {}
 	def add_resource(self, resource, amount):
+		#adds a resource to a players inventory
 		if resource.name in self.inventory:
 			self.inventory[resource.name] += amount
 		if resource.name not in self.inventory:
-			self.inventory.update(resource.name : amount)
+			self.inventory.update({resource.name : amount})
 class Building(object):
 	def __init__(self):
 		self.players = {}
@@ -125,6 +149,9 @@ class Map(object):
 		self.map = {}
 		self.squares = squares
 	def generate(self, size):
+		#generates a map of size x size full of random squares that can be on the map
+		#map is a dict full of number : number pairs naming the square
+		#each square must be properly initilized
 		t_size = float(size * size)
 		c_size = 0.0
 		cycle = 0
@@ -145,21 +172,27 @@ class Engine(object):
 		self.map = map
 		self.players = {}
 	def save(self):
+		#dumps save to a pickle file
 		pickle.dump(self.map, open(os.path.join("resources", "save.p"), 'wb'))
 	def load(self, map):
+		#loads save from a pickle file
 		self.map = map
 	def add_item(self, object, items):
+		# engine adds items to a thing (map square or building)
 		try:
 			items[0]
 		except:
 			print "Items must be a list"
 		for item in items:
 			object.items.update({item.name: items})
-	def add_player(self, square, player):
-		square.players.update({player.name: player})
-	def remove_player(self, square, player):
-		del square.players[player.name]
+	def add_player(self, object, player):
+		# adds player to a square or building
+		object.players.update({player.name: player})
+	def remove_player(self, object, player):
+		# removes player from square
+		del object.players[player.name]
 	def move(self, square, player, direction):
+		#checks if move is in valid square then adds player to that square and removes them from the old square
 		x, y = square.moves[direction].split(":")
 		if (x != 0) and (y != 0):
 			move = square.moves[direction]
@@ -169,20 +202,24 @@ class Engine(object):
 		else:
 			return False
 	def add_item(self, sqaure, player, item):
+		# add an item to a player
 		square.players[player.name].inventory.update({item.name: item})
 	def use_item(self, square, player, item):
 		square.players[player.name].inventory[item.name].use()
 		del square.players[player.name].inventory[item.name]
 	def spawn_player(self, player):
+		# spawns player on random sqaure
 		square = random.choice(self.map.map.values())
 		square.players.update({player.name: player})
 		player.pos = square.pos
 		self.players.update({player.name: player})
-	def destroy_player(self, player):
-		del self.players[player.name]
+	def destroy_player(self, square, player):
+		# for when a player leaves the game
+		del square.players[player.name]
 	def player_thread(self):
 		self.socket
 	def handout_resources(self):
+		# randomly hands out resources to sqaure based on objects in dict
 		for square in self.map.map:
 			for resource in self.map.map[square].resources:
 				resource.amount += random.randint(1, 10)
